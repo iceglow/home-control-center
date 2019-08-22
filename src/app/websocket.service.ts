b911f503ae0844d4a04e6dd7e0369de7a16d5de1
Joakim Lundin <joakim.lundin@bisnode.com>
2019-08-22 23:12:38 +0200

    Improved websocket service and added send method

--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { webSocket } from 'rxjs/webSocket';
 
-import {RxWebsocketSubject} from './RxWebsocketSubject';
 
 export interface EventHeader {
   ts: string;
@@ -19,18 +19,20 @@ export class WebsocketService {
 
   private eventSubject: Subject<Event>;
 
-  constructor() { }
-
   private connect(url): Subject<any> {
-    return new RxWebsocketSubject(url);
+    return webSocket(url);
   }
 
   public eventObserver(): Observable<Event> {
-    if (this.eventSubject == undefined) {
+    if (this.eventSubject === undefined) {
       this.eventSubject = <Subject<Event>>this
         .connect('ws://[SOME_WEBSOCKET]/ws/events'); // TODO: Get from config
     }
 
     return this.eventSubject;
+  }
+
+  send(msg) {
+    this.eventSubject.next(msg);
   }
 }
